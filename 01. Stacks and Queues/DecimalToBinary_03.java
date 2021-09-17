352a2aaf35e329aa9b68ee4664a28206042dbaf9
gabrielh00 <a11914741@univie.ac.at>
2021-09-17 12:49:01 +0200

    3. Lecture

---
+++ 01. Stacks and Queues/DecimalToBinary_03.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinary_03{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int input = Integer.parseInt(sc.nextLine());
+
+        if(input == 0){
+            System.out.println(0);
+        }
+
+        while(input != 0){
+            stack.push(input % 2);
+            input /= 2;
+        }
+
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop());
+        }
+    }
+}
