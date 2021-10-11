a64d4a31af9805b906ee19af209cf2d89b0171bb
gabrielh00 <a11914741@univie.ac.at>
2021-10-11 10:01:23 +0200

    01. Lab

---
+++ 07. Generics/GenericArrayCreator_02/Main.java
@@ -0,0 +1,15 @@
+import javax.crypto.CipherInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+
+        String[] strings = ArrayCreator.create(10, "im here");
+        Integer[] integers = ArrayCreator.create(5, 5);
+
+    }
+}
